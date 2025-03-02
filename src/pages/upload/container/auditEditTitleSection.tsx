312a1f61ed802e4320694056702555347326b305
Kangnets <kangnets88@gmail.com>
2025-03-03 01:47:33 +0900

    upload 리펙토링

--- src/pages/upload/container/auditEditTitleSection.tsx
+++ src/pages/upload/container/auditEditTitleSection.tsx
@@ -4,26 +4,35 @@ import { FilterDropDown } from "@/component/FilterDropdown/FilterDropdown";
 
 interface AuditEditTitleSectionProps {
   initialTitle?: string;
+  initialSubTitle?: string;
+  initialUserName?: string;
   initialCategory?: string;
   onTitleChange: (title: string) => void;
-  onSubTitleChange: (title: string) => void;
+  onSubTitleChange: (subTitle: string) => void;
+  onNameChange: (name: string) => void;
   onCategoryChange: (category: string) => void;
   categoryList: string[];
 }
 
 export function AuditEditTitleSection({
   initialTitle = "",
+  initialSubTitle = "",
+  initialUserName = "",
   initialCategory = "",
   onTitleChange,
   onSubTitleChange,
   onCategoryChange,
+  onNameChange,
   categoryList,
 }: AuditEditTitleSectionProps) {
   const [title, setTitle] = useState<string>(initialTitle);
-  const [subTitle, setSubTitle] = useState<string>(initialTitle);
+  const [subTitle, setSubTitle] = useState<string>(initialSubTitle);
+  const [userName, setUserName] = useState<string>(initialUserName);
   const [category, setCategory] = useState<string>(initialCategory);
 
   const truncatedTitle = useTruncateText(title, 50);
+  const truncatedSubTitle = useTruncateText(subTitle, 100);
+  const truncatedUserName = useTruncateText(userName, 25);
 
   const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const newTitle = event.target.value;
@@ -41,6 +50,14 @@ export function AuditEditTitleSection({
     }
   };
 
+  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newName = event.target.value;
+    if (newName.length <= 50) {
+      setUserName(newName);
+      onNameChange(newName);
+    }
+  };
+
   const handleCategoryChange = (value: string) => {
     setCategory(value);
     onCategoryChange(value);
@@ -54,16 +71,24 @@ export function AuditEditTitleSection({
           id="title"
           value={truncatedTitle}
           onChange={handleTitleChange}
-          className="w-full flex-1  rounded-xs border-[0.125rem] border-gray-300 px-3 py-[0.4rem] shadow-sm focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500 sm:text-sm"
-          placeholder="제목을 입력하세요"
+          className="w-3/12   rounded-xs border-[0.125rem] border-gray-300 px-3 py-[0.4rem] shadow-sm focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500 md:text-base sm:text-sm"
+          placeholder="글 제목"
         />
         <input
           type="text"
           id="subTitle"
-          value={truncatedTitle}
+          value={truncatedSubTitle}
           onChange={handleSubTitleChange}
-          className="w-full flex-1 rounded-xs border-[0.125rem] border-gray-300 px-3 py-[0.4rem] shadow-sm focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500 sm:text-sm"
-          placeholder="제목을 입력하세요"
+          className="w-8/12 rounded-xs border-[0.125rem] border-gray-300 px-3 py-[0.4rem] shadow-sm focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500 sm:text-sm"
+          placeholder="부제목"
+        />
+        <input
+          type="text"
+          id="name"
+          value={truncatedUserName}
+          onChange={handleNameChange}
+          className="w-1/12 rounded-xs border-[0.125rem] border-gray-300 px-3 py-[0.4rem] shadow-sm focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500 sm:text-sm"
+          placeholder="작성자"
         />
         <FilterDropDown
           className="sm-hidden xs-hidden flex h-[2.5rem] w-[6.5rem] items-center justify-center rounded-xs border-[0.125rem] border-gray-300 px-[0.4rem] py-[0.35rem] text-sm font-bold text-gray-500"
@@ -72,24 +97,6 @@ export function AuditEditTitleSection({
           optionValue={categoryList}
           onValueChange={handleCategoryChange}
           value={category}
-        />
-      </div>
-      <div className="mt-3 flex flex-row items-center justify-center gap-4 xs:flex-col xs:items-start xs:gap-1 sm:flex-col sm:items-start sm:gap-1">
-        <input
-          type="text"
-          id="title"
-          value={truncatedTitle}
-          onChange={handleTitleChange}
-          className="w-10/12 flex-1 rounded-xs border-[0.125rem] border-gray-300 px-3 py-[0.4rem] shadow-sm focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500 sm:text-sm"
-          placeholder="제목을 입력하세요"
-        />
-        <input
-          type="text"
-          id="subTitle"
-          value={truncatedTitle}
-          onChange={handleSubTitleChange}
-          className="w-2/12 flex-1 rounded-xs border-[0.125rem] border-gray-300 px-3 py-[0.4rem] shadow-sm focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500 sm:text-sm"
-          placeholder="제목을 입력하세요"
         />
       </div>
     </div>
